e245acf39f2399af399070c7c3d51449593e5938
Y302467 <yakubhafsah302@gmail.com>
2026-07-11 02:36:02 -0100

    Add intra.js assignment

---
+++ intra.js
@@ -0,0 +1,8 @@
+const name = "Hafsah";
+const age = 25;// Replace with your actual age
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Favorite Language:",favoriteLanguage);
+
