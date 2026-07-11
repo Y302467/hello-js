5d03ce80b7ed2bac5d5f70f148a7458743df4966
Y302467 <yakubhafsah302@gmail.com>
2026-07-11 01:29:50 -0100

    initial commit: add intro.js

--- intro.js
+++ intro.js
@@ -1,8 +1,8 @@
-  const name = "Hafsah";
-  const age = 25; // Replace with your actual age
-  const favoriteLanguage = "javaScript";
+const name = "Ama";
+const age = 25;// Replace with your actual age
+const favoriteLanguage = "JavaScript";
 
-  console.log("Name:", name);
-  console.log("Age:", age);
-  console.log("Favorite Programming Language:",favoriteLanguage);
- 
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Favorite Language:",favoriteLanguage);
+
